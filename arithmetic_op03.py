b8d20aba64f59a05d2e3c1e94db561be245eb894
Bahtee <bahtiyorjon_89@mail.ru>
2022-12-17 21:01:15 +0500

    a_3_ish

--- arithmetic_op03.py
+++ arithmetic_op03.py
@@ -1,5 +1,6 @@
 #Create a variable called 'number' and assign it the two-digit number
-
+number = 16
 #Find the reverse of the number and assign it to a variable called 'answer'.
-
+answer = 61
 #Print the value of the 'answer'
+print(answer)
