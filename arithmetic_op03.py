3057c1cfa282182b9bec8f0b1cbee3b98e62e46d
Bahtee <bahtiyorjon_89@mail.ru>
2022-12-19 22:43:54 +0500

    arifmetik-3

--- arithmetic_op03.py
+++ arithmetic_op03.py
@@ -1,6 +1,8 @@
 #Create a variable called 'number' and assign it the two-digit number
 number = 16
 #Find the reverse of the number and assign it to a variable called 'answer'.
-answer = 61
+a = number % 10
+b = number // 10
+answer = a*10 + b 
 #Print the value of the 'answer'
 print(answer)
